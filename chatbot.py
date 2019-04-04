80f80c5558282b067c299f854d247ee996296edd
Santiago Bermúdez <sabefo@Santiagos-MacBook-Pro.local>
2019-04-04 16:05:56 +0200

    chatbot sends to telegram meli answer

--- chatbot.py
+++ chatbot.py
@@ -11,20 +11,22 @@ import urllib
 
 class Chatbot():
     TOKEN = "639639336:AAEQMqogeObn3k0Y9ztD2L-GshGJdzcekr4"
-    mercado_libre = None
+    meli = None
 
     def manageMessage(self, msg):
         content_type, chat_type, chat_id = telepot.glance(msg)
         self.chat_id = chat_id
-        self.bot.sendMessage(self.chat_id, msg['text'])
+        self.meli = mercado_libre.MercadoLibre()
+        meli_answer = self.meli.formatJSON(self.meli.searchProduct(msg['text']))
+        self.bot.sendMessage(self.chat_id, meli_answer if meli_answer else 'no hay')
+        # self.bot.sendMessage(self.chat_id, msg['text'])
+        # self.bot.sendPhoto(chat_id, meli_answer[0]['photo']);
 
     def __init__(self):
         super(Chatbot, self).__init__()
 
         self.bot = telepot.Bot(self.TOKEN)
         print("getting updates")
-        # self.mercado_libre = mercado_libre.MercadoLibre()
-        # print(self.mercado_libre.searchProduct('chromecast'))
         MessageLoop(self.bot, self.manageMessage).run_as_thread()
 
         while True:
