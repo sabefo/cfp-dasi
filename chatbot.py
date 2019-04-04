341bdb633f3bdb693255bb19ed51ac3fa3d2ba14
Santiago Bermúdez <sabefo@Santiagos-MacBook-Pro.local>
2019-04-04 15:09:12 +0200

    bot mirrors user's messages

--- chatbot.py
+++ chatbot.py
@@ -12,23 +12,20 @@ import urllib
 class Chatbot():
     TOKEN = "639639336:AAEQMqogeObn3k0Y9ztD2L-GshGJdzcekr4"
     mercado_libre = None
-    # URL = "https://api.telegram.org/bot" + TOKEN
 
     def manageMessage(self, msg):
         content_type, chat_type, chat_id = telepot.glance(msg)
         self.chat_id = chat_id
-
-        self.message = msg
-        print(msg)
+        self.bot.sendMessage(self.chat_id, msg['text'])
 
     def __init__(self):
         super(Chatbot, self).__init__()
 
         self.bot = telepot.Bot(self.TOKEN)
         print("getting updates")
-        self.mercado_libre = mercado_libre.MercadoLibre()
-        print(self.mercado_libre.searchProduct('chromecast'))
-        # MessageLoop(self.bot, self.manageMessage).run_as_thread()
+        # self.mercado_libre = mercado_libre.MercadoLibre()
+        # print(self.mercado_libre.searchProduct('chromecast'))
+        MessageLoop(self.bot, self.manageMessage).run_as_thread()
 
         while True:
             time.sleep(5)
