ddddc7d943b5010e279ca9ac004d5c660cbd9ed4
Gonzalo Machado <gonzalo.machado@gmail.com>
2019-04-08 16:24:57 +0200

    Envio de data a ML

--- chatbot.py
+++ chatbot.py
@@ -11,51 +11,59 @@ import telepot
 from telepot.loop import MessageLoop
 import dialogflow_v2 as dialogflowAPI
 import dialogflow
+import os
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "C:/Users/Gonzalo Machado/Desktop/dasi/cfp-dasi/halogen-oxide-225816-facf749e60d7.json"
 
 DIALOGFLOW_PROJECT_ID = 'halogen-oxide-225816'
 GOOGLE_APPLICATION_CREDENTIALS = 'halogen-oxide-225816-facf749e60d7.json'
 SESSION_ID = 'proyectodasiucm'
 
 class Chatbot():
-    #TOKEN = "639639336:AAEQMqogeObn3k0Y9ztD2L-GshGJdzcekr4" # @Santi
-    meli = None
+	#TOKEN = "639639336:AAEQMqogeObn3k0Y9ztD2L-GshGJdzcekr4" # @Santi
+	meli = None
 
-    def __init__(self):
-        TOKEN = '894407782:AAHlyE4ko1wbWlj_oU-utzzBI0weSkC-4Pk' # @Gonzalo
-        session_client = dialogflowAPI.SessionsClient()
-        session = session_client.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
-        self.userConversation = dialogflow.testingDialogflow()
-        self.bot = telepot.Bot(TOKEN)
-        MessageLoop(self.bot, self.manageMessage).run_as_thread()
-        print('Listening ...')
-
-
-        # Keep the program running.
-        while 1:
-            time.sleep(10)
+	def __init__(self):
+		TOKEN = '894407782:AAHlyE4ko1wbWlj_oU-utzzBI0weSkC-4Pk' # @Gonzalo
+		session_client = dialogflowAPI.SessionsClient()
+		session = session_client.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
+		self.userConversation = dialogflow.testingDialogflow()
+		self.bot = telepot.Bot(TOKEN)
+		MessageLoop(self.bot, self.manageMessage).run_as_thread()
+		print('Listening ...')
 
 
-    def manageMessage(self, msg):
-        content_type, chat_type, chat_id = telepot.glance(msg)
-        self.chat_id = chat_id
-
-        # mensaje del usuario
-        self.message = msg
-
-        response = self.userConversation.detect_intent_texts(DIALOGFLOW_PROJECT_ID,SESSION_ID,self.message['text'],'es')
-        self.bot.sendMessage(self.chat_id, response)
-        """print(self.message['text'])"""
+		# Keep the program running.
+		while 1:
+			time.sleep(10)
 
 
-    def contactMercadoLibre(self, msg):
-        content_type, chat_type, chat_id = telepot.glance(msg)
-        self.chat_id = chat_id
-        self.meli = mercado_libre.MercadoLibre()
-        # meli_answer = self.meli.formatJSON(self.meli.searchProduct(msg['text']))
-        # self.bot.sendMessage(self.chat_id, meli_answer[0] if meli_answer else 'no hay')
-        # ============================================================
-        # self.bot.sendMessage(self.chat_id, msg['text'])
-        # self.bot.sendPhoto(chat_id, meli_answer[0]['photo']);
+	def manageMessage(self, msg):
+		content_type, chat_type, chat_id = telepot.glance(msg)
+		self.chat_id = chat_id
+
+		# mensaje del usuario
+		self.message = msg
+
+		response = self.userConversation.detect_intent_texts(DIALOGFLOW_PROJECT_ID,SESSION_ID,self.message['text'],'es')
+		self.bot.sendMessage(self.chat_id, response["responseText"])
+		if response["intent"] == "Compra" and response["allParams"]:
+			self.contactMercadoLibre(self.chat_id,response["searchText"])
+
+		"""print(self.message['text'])"""
+
+
+	def contactMercadoLibre(self, chat_id,msg):
+		self.meli = mercado_libre.MercadoLibre()
+		self.chat_id = chat_id
+		meli_answer = self.meli.formatJSON(self.meli.searchProduct(msg))
+		if meli_answer[0]:
+			self.bot.sendMessage(self.chat_id, meli_answer[0]["link"])
+		else:
+			"no hay"
+		# self.bot.sendMessage(self.chat_id, meli_answer[0] if meli_answer else 'no hay')
+		# self.bot.sendMessage(self.chat_id, msg)
+		# self.bot.sendPhoto(chat_id, meli_answer[0]['photo']);
 
 
 
@@ -63,4 +71,4 @@ class Chatbot():
 
 
 if __name__ == '__main__':
-    chatbot = Chatbot()
+	chatbot = Chatbot()
--- dialogflow.py
+++ dialogflow.py
@@ -15,15 +15,31 @@ class testingDialogflow:
 	def detect_intent_texts(self,project_id, session_id, texts, language_code):
 		session_client = dialogflow.SessionsClient()
 		session = session_client.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
-		print('Session path: {}\n'.format(session))
+		# print('Session path: {}\n'.format(session))
 		text_input = dialogflow.types.TextInput(text=texts, language_code=language_code)
 		query_input = dialogflow.types.QueryInput(text=text_input)
 		response = session_client.detect_intent(session=session, query_input=query_input)
-		print('=' * 20)
-		print('Query text: {}'.format(response.query_result.query_text))
-		print('Detected intent: {} (confidence: {})\n'.format(
-			response.query_result.intent.display_name,
-			response.query_result.intent_detection_confidence))
-		print('Fulfillment text: {}\n'.format(
-			response.query_result.fulfillment_text))
-		return response.query_result.fulfillment_text
+		intentName = response.query_result.intent.display_name
+		allParams = response.query_result.all_required_params_present
+		fulfillmentText = response.query_result.fulfillment_text
+		paramBienes = response.query_result.parameters.fields["Bienes"].string_value
+		paramMarca = response.query_result.parameters.fields["Marca"].string_value
+		searchFor = paramBienes + " " + paramMarca
+		responseDialogFlow = {
+		"intent": intentName,
+		"allParams": allParams,
+		"searchText": searchFor,
+		"responseText": fulfillmentText
+		}
+		# print(searchFor)
+
+
+		# print(intentName, finalResponse)
+		# print('=' * 20)
+		# print('Query text: {}'.format(response.query_result.query_text))
+		# print('Detected intent: {} (confidence: {})\n'.format(
+		# 	response.query_result.intent.display_name,
+		# 	response.query_result.intent_detection_confidzence))
+		# print('Fulfillment text: {}\n'.format(
+		# 	response.query_result.fulfillment_text))
+		return responseDialogFlow
