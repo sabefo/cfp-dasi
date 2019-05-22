12941eb0e6fb6d047377862b2539de2d2b6e514f
sabefo <santiagofortes@gmail.com>
2019-05-22 13:04:23 +0200

    rule for account balance done

--- chatbot.py
+++ chatbot.py
@@ -1,15 +1,17 @@
 # coding=utf-8
+import dialogflow
+import dialogflow_v2 as dialogflowAPI
 import json
 import mercado_libre
+import os
 import pprint
+from pyknow import *
 import requests
+import rules
 import telepot
 from telepot.loop import MessageLoop
 import time
 import urllib
-import dialogflow_v2 as dialogflowAPI
-import dialogflow
-import os
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "halogen-oxide-225816-facf749e60d7.json"
 
@@ -17,22 +19,27 @@ DIALOGFLOW_PROJECT_ID = 'halogen-oxide-225816'
 GOOGLE_APPLICATION_CREDENTIALS = 'halogen-oxide-225816-facf749e60d7.json'
 SESSION_ID = 'proyectodasiucm'
 
+
 class Chatbot():
-    #TOKEN = "639639336:AAEQMqogeObn3k0Y9ztD2L-GshGJdzcekr4" # @Santi
+    # TOKEN = "639639336:AAEQMqogeObn3k0Y9ztD2L-GshGJdzcekr4" # @Santi
+    chatBotRules = None
     meli = None
+    userConversation = None
 
     def __init__(self):
-        self.eatBotRules.setBot(self)
         TOKEN = '894407782:AAHlyE4ko1wbWlj_oU-utzzBI0weSkC-4Pk' # @Gonzalo
-        session_client = dialogflowAPI.SessionsClient()
-        session = session_client.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
-        self.userConversation = dialogflow.testingDialogflow()
         self.bot = telepot.Bot(TOKEN)
+
         MessageLoop(self.bot, self.manageMessage).run_as_thread()
         print('Listening ...')
 
+        self.chatBotRules = rules.ChatBotRules()
+        self.chatBotRules.setBot(self)
 
-        # Keep the program running.
+        session_client = dialogflowAPI.SessionsClient()
+        session = session_client.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
+        self.userConversation = dialogflow.testingDialogflow()
+
         while 1:
             time.sleep(10)
 
@@ -43,14 +50,27 @@ class Chatbot():
         # mensaje del usuario
         self.message = msg
 
-        response = self.userConversation.detect_intent_texts(DIALOGFLOW_PROJECT_ID,SESSION_ID,self.message['text'],'es')
+        response = self.userConversation.detect_intent_texts(DIALOGFLOW_PROJECT_ID, SESSION_ID, self.message['text'], 'es')
+        print(response)
+
+        self.chatBotRules.reset()
+
+        if response['intent'] != '':
+            self.chatBotRules.declare(Fact(intent = response['intent']))
+        print(self.chatBotRules.facts)
+
+        # ejecutamos el motor de reglas
+        self.chatBotRules.run()
+
         self.bot.sendMessage(self.chat_id, response["responseText"])
+        # if response["intent"] == "Balance":
+        #     print("PRINT QUE DEBERIA DAR EL BALANCE DE LA CUENTA")
         if response["intent"] == "Compra" and response["allParams"]:
-            self.contactMercadoLibre(self.chat_id,response["searchText"])
+            self.contactMercadoLibre(self.chat_id, response["searchText"])
 
         """print(self.message['text'])"""
 
-    def contactMercadoLibre(self, chat_id,msg):
+    def contactMercadoLibre(self, chat_id, msg):
         self.meli = mercado_libre.MercadoLibre()
         self.chat_id = chat_id
         meli_answer = self.meli.formatJSON(self.meli.searchProduct(msg))
@@ -61,6 +81,10 @@ class Chatbot():
         # self.bot.sendMessage(self.chat_id, meli_answer[0] if meli_answer else 'no hay')
         # self.bot.sendMessage(self.chat_id, msg)
         # self.bot.sendPhoto(chat_id, meli_answer[0]['photo']);
+
+    def responseBalance(self):
+        # Dar balance de cuenta al usuario
+        self.bot.sendMessage(self.chat_id, 'Aqui debería habrer una funcion que devolviera el balance de la cuenta con código')
 
 
 if __name__ == '__main__':
--- rules.py
+++ rules.py
@@ -1,19 +1,46 @@
+# coding=utf-8
 from pyknow import *
 
-class EatBotRules(KnowledgeEngine):
+
+class ChatBotRules(KnowledgeEngine):
     bot = None
 
     def setBot(self, bot):
         self.bot = bot
 
+    @Rule(Fact(intent='Balance'))
+    def ruleGiveBalance(self):
+        # quiero el balance
+        self.bot.responseBalance()
 
-    @Rule(Fact(intent='Default Fallback Intent'))
+    @Rule(Fact(intent='Movimiento - Ingreso'))
     def ruleNotUnderstood(self):
         # no te entiendo
-        self.bot.responseNotUnderstood()
+        self.bot.responseIncome()
 
-    @Rule(Fact(intent='greet'))
-    def ruleGreet(self):
-        # saludar al usuario
-        name = self.bot.message['from']['first_name']
-        self.bot.responseGreet(name)
+# from pyknow import *
+
+# class Greetings(KnowledgeEngine):
+#     @DefFacts()
+#     def _initial_action(self):
+#         yield Fact(action="greet")
+
+#     @Rule(Fact(action='greet'),
+#         NOT(Fact(name=W())))
+#     def ask_name(self):
+#         self.declare(Fact(name=input("What's your name? ")))
+
+#     @Rule(Fact(action='greet'),
+#         NOT(Fact(location=W())))
+#     def ask_location(self):
+#         self.declare(Fact(location=input("Where are you? ")))
+
+#     @Rule(Fact(action='greet'),
+#         Fact(name=MATCH.name),
+#         Fact(location=MATCH.location))
+#     def greet(self, name, location):
+#         print("Hi %s! How is the weather in %s?" % (name, location))
+
+# engine = Greetings()
+# engine.reset() # Prepare the engine for the execution.
+# engine.run() # Run it!
